943d65b921681ffcfd83d475599e1ee53a0b614f
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-02-06 14:58:39 +0000

    tuning the config template

--- config/config-template.php
+++ config/config-template.php
@@ -1,29 +1,30 @@
 <?php
 /* 
- * The configuration of wikiplex
- * 
- * 
+ * The configuration of Foodle.
+ *
+ * Read more about foodle here: 
+ *  http://rnd.feide.no/content/foodle-0
  */
 
 $config = array (
 
-	'simplesamlphpdir' 		=> NULL,
-	'basedir' => '/www/foodle.feide.no/',
-	'baseurlpath' => '',
+	'simplesamlphpdir'   => NULL,
+	'basedir'            => '/www/foodle.feide.no/',
+	'baseurlpath'        => '',
 	
-	'templatedir'           => 'templates/',
-	'dictionarydir' 		=> 'dictionaries/',
+	'templatedir'        => 'templates/',
+	'dictionarydir'      => 'dictionaries/',
 	/*
 	 * Languages available and what language is default
 	 */
-	'language.available'	=> array('en', 'no', 'da', 'sv', 'nl', 'de', 'es', 'sl'),
-	'language.default'		=> 'en',
+	'language.available'  => array('en', 'no', 'da', 'sv', 'nl', 'de', 'es', 'sl'),
+	'language.default'	  => 'en',
 	
 	'adminUsers' => array('andreas@rnd.feide.no', 'andreas@uninett.no'),
 	
 	'db.host'	=> 'sql.example.org',
+	'db.name'	=> 'feidefoodle'
 	'db.user'	=> 'user',
 	'db.pass'	=> 'pass',
-	'db.name'	=> 'feideopenwiki'
 
 );
