1a43687547e4e7c204dc72a5f132b00151ea8ab0
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2011-01-25 13:44:10 +0000

    Timer class logging turned off

--- lib/Timer.php
+++ lib/Timer.php
@@ -8,13 +8,15 @@ class Timer {
 
 	public static function start() {
 		self::$last = microtime(TRUE);
-		error_log('start ' . $last);
+// 		error_log('start ' . self::$last);
 	}
 
 	public static function tick($tag) {
 
+		if (!self::$last) self::start();
+
 		$now = microtime(TRUE);
-		error_log('tick ' . $now . ' '. $tag);
+		// error_log('tick ' . $now . ' '. $tag);
 		self::$list[] = array(  ((float)$now - (float)self::$last), $tag);
 		self::$last = $now;
 	}
