043ddc7575feda9d875990a2dbf1902bc3206413
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2011-02-02 06:55:19 +0000

    fix for getsatisfaction... still not working perfect...

--- lib/Pages/PageGS.php
+++ lib/Pages/PageGS.php
@@ -16,7 +16,10 @@ class Pages_PageGS extends Pages_Page {
 	
 	// Authenticate the user
 	private function auth() {
-		$this->auth = new FoodleAuth();
+	
+#		echo '<pre>'; print_r($this); exit;
+	
+		$this->auth = new FoodleAuth($this->fdb);
 		$this->auth->requireAuth(FALSE);
 
 		$this->user = $this->auth->getUser();
@@ -32,26 +35,10 @@ class Pages_PageGS extends Pages_Page {
 		$url = FastPass::url($this->config->getValue('getsatisfaction.key'), $this->config->getValue('getsatisfaction.secret'), 
 			$this->user->email, $this->user->name, $this->user->userid);
 
-#		setcookie("fastpass", $url, time()+60*60*24*30);
-
 		$furl = 'http://tjenester.ecampus.no/fastpass/finish_signover?company=ecampus&fastpass=' . urlencode($url);
 		
 		SimpleSAML_Utilities::redirect($furl);
 
-// 		echo '<html><body><img onload="" />
-// 			<script src="' . $url . '" type="text/javascript"></script>
-// 			<script type="text/javascript">window.close();</script>
-// 			</body></html>';
-
-// 		
-// 	exit;
-// 		echo '<pre>Request: '; 
-// 		print_r($_REQUEST);
-// 		echo '</pre>';
-// 				
-// 		echo 'url: ' . $url;
-// 		
-// 		exit;
 
 	}
 	
