5b4a23560a36591a20c9f0a1c37c4e5bcc633897
Andreas Aakre Solberg <andreas.solberg@uninett.no>
2011-11-30 20:03:22 +0100

    Adding support for extra fields in CSV output

--- lib/Pages/CSVFoodle.php
+++ lib/Pages/CSVFoodle.php
@@ -21,19 +21,49 @@ class Pages_CSVFoodle extends Pages_Page {
 	// Process the page.
 	function show() {
 
+		$s = ';';
 
 		$url = FoodleUtils::getUrl() . 'foodle/' . $this->foodle->identifier;
 
 		$responses = $this->foodle->getResponses();
 		
 		session_cache_limiter('public'); 
+		
+		$extra = $this->foodle->getExtraFields();
+
 
 		header("Content-type: text/csv; charset=utf-8");
 		header("Content-disposition:  attachment; filename=foodle-" . $this->foodle->identifier . "_" . date("Y-m-d") . ".csv");
 
-		$s = ';';
+
 		foreach ($responses AS $response) {
-			echo $response->username . $s .  $response->userid . $s . join($s, $response->response['data']) . $s . date("Y-m-d H:i", $response->created) . "\r\n";
+
+			$extrastr = '';
+			$user = $this->fdb->readUser($response->userid);
+			
+			if (!empty($user) && !empty($extra)) {
+				foreach($extra AS $e) {
+					switch($e) {
+						case 'org':
+							$extrastr .= $user->org . $s;
+							break;
+
+						case 'location':
+							$extrastr .= $user->location . $s;
+							break;
+
+						case 'timezone':
+							$extrastr .= $user->timezone . $s;
+							break;
+
+						default:
+					}
+
+				}
+			}
+
+	
+			echo $response->username . $s .  $response->userid . $s . $extrastr . join($s, $response->response['data']) . $s . date("Y-m-d H:i", $response->created) . "\r\n";
 		}
 
 	}
