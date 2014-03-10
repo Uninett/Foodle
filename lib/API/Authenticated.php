64973d620043317cd45ccad950c47ef0e8b6ea25
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2014-03-10 19:57:48 +0100

    Disabling OAuth

--- lib/API/Authenticated.php
+++ lib/API/Authenticated.php
@@ -9,27 +9,27 @@ abstract class API_Authenticated extends API_API {
 		parent::__construct($config, $parameters);
 	}
 	
-	protected function requireOAuth() {
+	// protected function requireOAuth() {
 
-		$store = new sspmod_oauth_OAuthStore();
-		$server = new sspmod_oauth_OAuthServer($store);
+	// 	$store = new sspmod_oauth_OAuthStore();
+	// 	$server = new sspmod_oauth_OAuthServer($store);
 		
-		$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
-		$plaintext_method = new OAuthSignatureMethod_PLAINTEXT();
+	// 	$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
+	// 	$plaintext_method = new OAuthSignatureMethod_PLAINTEXT();
 		
-		$server->add_signature_method($hmac_method);
-		$server->add_signature_method($plaintext_method);
+	// 	$server->add_signature_method($hmac_method);
+	// 	$server->add_signature_method($plaintext_method);
 		
-		$req = OAuthRequest::from_request();
-		list($consumer, $token) = $server->verify_request($req);
+	// 	$req = OAuthRequest::from_request();
+	// 	list($consumer, $token) = $server->verify_request($req);
 		
-		$data = $store->getAuthorizedData($token->key);
+	// 	$data = $store->getAuthorizedData($token->key);
 		
-		$userid = FoodleAuth::getUserid($data);
-		if (empty($userid)) throw new Exception('User ID not found in stored authenticated session. Should not happen.');
-		$this->user = $this->fdb->readUser($userid);
+	// 	$userid = FoodleAuth::getUserid($data);
+	// 	if (empty($userid)) throw new Exception('User ID not found in stored authenticated session. Should not happen.');
+	// 	$this->user = $this->fdb->readUser($userid);
 
-	}
+	// }
 
 	protected function optionalAuth() {
 		$this->auth = new FoodleAuth($this->fdb);
@@ -58,7 +58,7 @@ abstract class API_Authenticated extends API_API {
 			return;
 		}
 		
-		$this->requireOAuth();
+		// $this->requireOAuth();
 		
 	}
 
