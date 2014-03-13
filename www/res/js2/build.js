1ca47f622aa3e627171582d90ead7ff51e0e3170
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2014-03-13 13:57:02 +0100

    Adding experimental main js build

---
+++ www/res/js2/build.js
@@ -0,0 +1,17 @@
+({
+    baseUrl: ".",
+    paths: {
+		"jquery": "lib/jquery-1.11.0.min",
+		
+        "bootstrap": "../uninett-theme-bootstrap/js",   
+        "bootstrap-datepicker": "lib/bootstrap-datepicker",
+        
+        "typeahead": "lib/bootstrap3-typeahead",
+
+        "moment": "lib/moment",
+        "moment-timezone": "lib/moment-timezone",
+        "moment-timezone-data": "lib/moment-timezone-data"
+    },
+    name: "main",
+    out: "main.build.js"
+})
